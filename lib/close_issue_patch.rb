4eac2acd59d9acab50379eabe3b92cbbc87797f9
FJ Martinez <fjmartinez@tedial.com>
2011-07-14 00:32:08 -0700

    Only change the closed_date when change status appear. Fixing the bug with the right-click menu.

--- lib/close_issue_patch.rb
+++ lib/close_issue_patch.rb
@@ -1,23 +1,23 @@
 module ClosedDateIssue
   class Hooks < Redmine::Hook::ViewListener
     def controller_issues_new_before_save(context)
-      save_closed_date(context)
+      if context[:issue] and context[:issue].status.is_closed? then
+        context[:issue].closed_date = Time.now
+      end
     end
 
     def controller_issues_edit_before_save(context)
-      save_closed_date(context)
-    end
-
-    def save_closed_date(context)
-      if(context[:issue])
-        closed_statuses = IssueStatus.find(:all, :conditions => { :is_closed => true })
-        find = closed_statuses.find_all { |obj| obj.id == context[:issue].status.id}
-        if(context[:issue].status.id ==  closed_statuses or (closed_statuses.instance_of? Array and find.size() > 0))
-          context[:issue].closed_date = Time.now
-        else
-          context[:issue].closed_date = nil if(context[:issue].closed_date)
+      if context[:issue] then
+        @issue = Issue.find context[:issue].id
+        unless @issue.status.id == context[:issue].status.id then # CHANGED STATUS
+          if context[:issue].status.is_closed?
+            context[:issue].closed_date = Time.now
+          else
+            context[:issue].closed_date = nil if(context[:issue].closed_date)
+          end
         end
       end
     end
+    alias_method :controller_issues_bulk_edit_before_save, :controller_issues_edit_before_save
   end
 end
